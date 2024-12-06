96e7bb45893b5e14faa2b80fe17e8a38d04e25f1
Hoang Phi <69hoangphi69@gmail.com>
2024-12-06 17:44:21 +0700

    updated navigation bar ui

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -25,12 +25,30 @@ export default function Navigation() {
   ];
 
   return (
-    <nav className="flex justify-center items-center gap-4 p-4">
-      {links.map((link, index) => (
-        <Link key={index} href={link.route}>
-          {link.displayName}
-        </Link>
-      ))}
+    <nav className="top-0 z-10 sticky flex justify-between items-center gap-8 bg-dark-1/20 backdrop-blur-xl backdrop-saturate-150 md:px-16 isolate">
+      <Link href="/">
+        <img
+          src={'/logo/logo-text.svg'}
+          alt="Levia logo"
+          className="block md:m-0 ml-5 max-w-20 object-contain"
+        />
+      </Link>
+
+      <ul className="md:flex items-center hidden">
+        {links.map((link, index) => (
+          <Link
+            className="px-4 py-4 font-bold text-light-1 text-nowrap text-sm hover:underline decoration-2 decoration-accent-2 fill-light-1"
+            key={index}
+            href={link.route}
+          >
+            {link.displayName}
+          </Link>
+        ))}
+      </ul>
+
+      <Link href="/contacts" className="text-nowrap text-sm btn">
+        Liên hệ
+      </Link>
     </nav>
   );
 }
