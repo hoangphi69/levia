3662e241548919d9180583eee44e751960f4d158
Hoang Phi <69hoangphi69@gmail.com>
2024-12-08 10:53:50 +0700

    updated type safety for product card component

--- src/app/components/product-card.tsx
+++ src/app/components/product-card.tsx
@@ -1,22 +1,22 @@
-export default function ProductCard({
-  image,
-  title,
-  price,
-}: {
-  image: string;
-  title: string;
-  price: number;
-}) {
-  const formattedPrice = Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'vnd',
-  }).format(price);
+import type { Product } from '@prisma/client';
 
+type ProductCard = Pick<Product, 'title' | 'price'> & { image: string };
+
+export default function ProductCard({ image, title, price }: ProductCard) {
   return (
     <article>
       <img className="w-full" src={image} alt="" />
       <p className="mt-4 font-bold text-lg">{title}</p>
-      <p>từ {formattedPrice}</p>
+      <p>từ {formattedPrice(price)}</p>
     </article>
   );
+}
+
+function formattedPrice(price: number | null) {
+  if (!price) return 'N/A';
+
+  return price.toLocaleString('vi-VN', {
+    style: 'currency',
+    currency: 'vnd',
+  });
 }
