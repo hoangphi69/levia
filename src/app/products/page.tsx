c74918e4f396224f121b57adf272736396bc5219
Hoang Phi <69hoangphi69@gmail.com>
2024-12-09 16:35:50 +0700

    added breadcrumb for products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,67 +1,35 @@
 import Link from 'next/link';
 import ProductCard from '../components/product-card';
 import prisma from '../lib/prisma';
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb';
 
 export default async function Products() {
   const title = 'Sản phẩm';
   const subtitle = 'Khám phá các dòng sản phẩm mới trong căn bếp';
-  // const products = [
-  //   {
-  //     id: '821j898qwef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 16000000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '821j89qwef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 1600000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '821j88qwef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 6900000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '821j898qwf',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 16000000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '821j898qef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 69000000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '81j898qwef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 16000000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '82j898qwef',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 5600000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  //   {
-  //     id: '821j898qwe',
-  //     title: 'Bếp từ đôi LV-3000',
-  //     price: 12000000,
-  //     image: 'https://picsum.photos/250',
-  //   },
-  // ];
-
   const products = await prisma.product.findMany();
 
   return (
     <div className="p-4">
       <div className="space-y-4">
-        <span>Trang chủ - Sản phẩm</span>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Trang chủ</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Sản phẩm</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
         <h1 className="font-bold text-4xl">{title}</h1>
         <p>{subtitle}</p>
       </div>
