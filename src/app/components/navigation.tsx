3db69702c83b2c1c94f7c663e1829af3cf2598da
Hoang Phi <69hoangphi69@gmail.com>
2025-01-07 09:16:49 +0700

    updated responsive navigation menu

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,6 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import SmoothImage from './smooth-image';
+import logo from '../../../public/logo/logo-text.svg';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 
 export default function Navigation() {
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
   const links = [
     {
       route: '/',
@@ -13,6 +28,16 @@ export default function Navigation() {
     {
       route: '/products',
       displayName: 'Sản phẩm',
+      dropdown: (
+        <div className="*:inline-flex flex flex-col *:items-center *:gap-2 *:px-6 *:py-4 font-semibold text-muted-foreground whitespace-nowrap">
+          <Link href={'/products/'}>
+            Bếp điện từ{' '}
+            <span className="material-symbols-outlined">chevron_right</span>
+          </Link>
+          <Link href={'/products/'}>Máy hút mùi</Link>
+          <Link href={'/products/'}>Đồ gia dụng</Link>
+        </div>
+      ),
     },
     {
       route: '/articles',
@@ -24,31 +49,89 @@ export default function Navigation() {
     },
   ];
 
-  return (
-    <nav className="top-0 z-10 sticky flex justify-between items-center gap-8 bg-dark-1/20 backdrop-blur-xl backdrop-saturate-150 md:px-16 isolate">
+  return isDesktop ? (
+    <nav className="top-0 z-50 sticky flex justify-between items-center gap-8 bg-secondary/20 hover:bg-secondary backdrop-blur-xl backdrop-saturate-150 px-16 transition-colors isolate">
       <Link href="/">
-        <img
-          src={'/logo/logo-text.svg'}
-          alt="Levia logo"
-          className="block md:m-0 ml-5 max-w-20 object-contain"
-        />
+        <SmoothImage src={logo} alt="Levia logo" className="max-w-20" />
       </Link>
 
       <ul className="md:flex items-center hidden">
         {links.map((link, index) => (
-          <Link
-            className="px-4 py-4 font-bold text-light-1 text-nowrap text-sm hover:underline decoration-2 decoration-accent-2 fill-light-1"
-            key={index}
-            href={link.route}
-          >
-            {link.displayName}
-          </Link>
+          <li className="relative group" key={index}>
+            {link.dropdown ? (
+              // Link with dropdown
+              <>
+                <Link
+                  className="inline-flex items-center gap-1 p-4 font-bold text-sm whitespace-nowrap"
+                  href={link.route}
+                >
+                  <span className="group-hover:underline decoration-2 decoration-accent-gold">
+                    {link.displayName}
+                  </span>
+                  <span className="group-hover:rotate-180 group-hover:text-accent-gold transition-transform material-symbols-outlined">
+                    keyboard_arrow_down
+                  </span>
+                </Link>
+                <div className="group-hover:block top-full left-1/2 absolute hidden bg-secondary shadow-lg -translate-x-1/2">
+                  {link.dropdown}
+                </div>
+              </>
+            ) : (
+              // Link without dropdown
+              <Link
+                className="p-4 font-bold text-sm hover:underline whitespace-nowrap decoration-2 decoration-accent-gold"
+                href={link.route}
+              >
+                {link.displayName}
+              </Link>
+            )}
+          </li>
         ))}
       </ul>
 
-      <Link href="/contacts" className="text-nowrap text-sm btn">
-        Liên hệ
+      <Button
+        className="bg-gradient-to-tr from-accent-gold to-accent-almond rounded-none font-bold"
+        asChild
+      >
+        <Link href="/contacts">Liên hệ</Link>
+      </Button>
+    </nav>
+  ) : (
+    <nav className="top-0 z-50 sticky flex justify-between items-center gap-8 bg-secondary/20 hover:bg-secondary backdrop-blur-xl backdrop-saturate-150 px-6 py-2 transition-colors isolate">
+      <Link href="/">
+        <SmoothImage src={logo} alt="Levia logo" className="max-w-20" />
       </Link>
+
+      <div className="flex items-center gap-6">
+        <Button
+          className="bg-gradient-to-tr from-accent-gold to-accent-almond rounded-none font-bold"
+          asChild
+        >
+          <Link href="/contacts">Liên hệ</Link>
+        </Button>
+        <Sheet>
+          <SheetTrigger className="content-center grid">
+            <span className="material-symbols-outlined">menu</span>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <VisuallyHidden.Root>
+              <SheetTitle>Menu</SheetTitle>
+            </VisuallyHidden.Root>
+            <ul className="">
+              {links.map((link, index) => (
+                <li className="hover:bg-secondary" key={index}>
+                  <Link
+                    className="flex px-6 py-4 font-semibold uppercase"
+                    href={link.route}
+                  >
+                    {link.displayName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   );
 }
