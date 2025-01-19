97f109b2b43092cbf66cce2ee9e7188a41070472
Hoang Phi <69hoangphi69@gmail.com>
2025-01-19 16:58:08 +0700

    updated util functions

--- src/app/lib/utils.tsx
+++ src/app/lib/utils.tsx
@@ -33,4 +33,50 @@ const SplitedTitle: React.FC<TwoLineTitleProps> = ({
   );
 };
 
-export { SplitedTitle };
+function formattedPrice(price: number | null | undefined) {
+  if (!price) return 'N/A';
+
+  return price.toLocaleString('vi-VN', {
+    style: 'currency',
+    currency: 'vnd',
+  });
+}
+
+// This function is modified based on a StackOverflow answer
+// For more information please refer to: https://stackoverflow.com/a/53800501/28699807
+function formattedRelativeTime(date: Date): string {
+  const units: { [key: string]: number } = {
+    year: 24 * 60 * 60 * 1000 * 365,
+    month: (24 * 60 * 60 * 1000 * 365) / 12,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000,
+  };
+
+  const rtf = new Intl.RelativeTimeFormat('vi', { numeric: 'auto' });
+
+  const elapsed = date.getTime() - Date.now();
+
+  for (const unit in units) {
+    if (
+      Object.prototype.hasOwnProperty.call(units, unit) &&
+      (Math.abs(elapsed) > units[unit] || unit === 'second')
+    ) {
+      return rtf.format(
+        Math.round(elapsed / units[unit]),
+        unit as Intl.RelativeTimeFormatUnit
+      );
+    }
+  }
+
+  return ''; // Fallback if no match (this is unlikely to occur).
+}
+
+function formattedDate(date: Date | undefined): string {
+  if (!date) return 'N/A';
+
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+}
+
+export { SplitedTitle, formattedPrice, formattedRelativeTime, formattedDate };
