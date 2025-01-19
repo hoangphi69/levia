8251eff173d0ee40d9f9ff3e83c65f1fe9ac905d
Hoang Phi <69hoangphi69@gmail.com>
2025-01-19 17:09:37 +0700

    refactored agencies page's banner

--- src/app/agencies/page.tsx
+++ src/app/agencies/page.tsx
@@ -14,26 +14,35 @@ import AgencyCard from '../components/card-agency';
 export default async function Agencies() {
   const agencies = await prisma.agency.findMany();
 
+  const banner = {
+    breadcrumb: [
+      {
+        display: 'Trang chủ',
+        href: '/',
+      },
+      {
+        display: 'Đại lý',
+      },
+    ],
+    image: '../images/products-banner.png',
+    title: 'Hệ thống đại lý',
+    subtitle: 'Khám phá hệ thống đại lý trải rộng khắp vùng miền',
+  };
+
   return (
-    <>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Trang chủ</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator></BreadcrumbSeparator>
-          <BreadcrumbItem>
-            <BreadcrumbPage>Đại lý</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <main>
+      <Banner image={banner.image} breadcrumb={banner.breadcrumb}>
+        <header className="flex-1 content-center md:content-end space-y-2">
+          <h1 className="font-bold text-2xl text-center text-gradient md:text-left lg:text-4xl">
+            {banner.title}
+          </h1>
+          <p className="md:max-w-[25ch] text-base text-center text-muted-foreground md:text-left lg:text-lg tracking-wide">
+            {banner.subtitle}
+          </p>
+        </header>
+      </Banner>
 
-      <Banner
-        title={'Hệ thống đại lý'}
-        image={'https://picsum.photos/1280/350'}
-      />
-
-      <section>
+      <section className="md:px-16 md:py-12 p-6">
         <ul>
           {agencies.map(({ id, title, image, location, phone, email }) => (
             <AgencyCard
@@ -47,6 +56,6 @@ export default async function Agencies() {
           ))}
         </ul>
       </section>
-    </>
+    </main>
   );
 }
