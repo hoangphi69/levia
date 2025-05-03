30e6164ad9a02c94f44482dee1e48227342cbff8
Hoang Phi <69hoangphi69@gmail.com>
2025-05-03 15:40:07 +0700

    saved

--- src/app/(public)/(site)/layout.tsx
+++ src/app/(public)/(site)/layout.tsx
@@ -28,99 +28,99 @@ export const metadata: Metadata = {
 
 const shImogenAgnes = localFont({
   variable: '--font-sh-imogen-agnes',
-  src: 'ShImogenAgnes.woff',
+  src: '../../../fonts/SHImogenAgnes.woff',
 });
 
 const monorale = localFont({
   variable: '--font-monorale',
   src: [
     {
-      path: 'Monorale-Thin.woff',
+      path: '../../../fonts/Monorale-Thin.woff',
       weight: '100',
       style: 'normal',
     },
     {
-      path: 'Monorale-ThinItalic.woff',
+      path: '../../../fonts/Monorale-ThinItalic.woff',
       weight: '100',
       style: 'italic',
     },
     {
-      path: 'Monorale-ExtraLight.woff',
+      path: '../../../fonts/Monorale-ExtraLight.woff',
       weight: '200',
       style: 'normal',
     },
     {
-      path: 'Monorale-ExtraLightItalic.woff',
+      path: '../../../fonts/Monorale-ExtraLightItalic.woff',
       weight: '200',
       style: 'italic',
     },
     {
-      path: 'Monorale-Light.woff',
+      path: '../../../fonts/Monorale-Light.woff',
       weight: '300',
       style: 'normal',
     },
     {
-      path: 'Monorale-LightItalic.woff',
+      path: '../../../fonts/Monorale-LightItalic.woff',
       weight: '300',
       style: 'italic',
     },
     {
-      path: 'Monorale-Regular.woff',
+      path: '../../../fonts/Monorale-Regular.woff',
       weight: '400',
       style: 'normal',
     },
     {
-      path: 'Monorale-Italic.woff',
+      path: '../../../fonts/Monorale-Italic.woff',
       weight: '400',
       style: 'italic',
     },
     {
-      path: 'Monorale-Medium.woff',
+      path: '../../../fonts/Monorale-Medium.woff',
       weight: '500',
       style: 'normal',
     },
     {
-      path: 'Monorale-MediumItalic.woff',
+      path: '../../../fonts/Monorale-MediumItalic.woff',
       weight: '500',
       style: 'italic',
     },
     {
-      path: 'Monorale-SemiBold.woff',
+      path: '../../../fonts/Monorale-SemiBold.woff',
       weight: '600',
       style: 'normal',
     },
     {
-      path: 'Monorale-SemiBoldItalic.woff',
+      path: '../../../fonts/Monorale-SemiBoldItalic.woff',
       weight: '600',
       style: 'italic',
     },
     {
-      path: 'Monorale-Bold.woff',
+      path: '../../../fonts/Monorale-Bold.woff',
       weight: '700',
       style: 'normal',
     },
     {
-      path: 'Monorale-BoldItalic.woff',
+      path: '../../../fonts/Monorale-BoldItalic.woff',
       weight: '700',
       style: 'italic',
     },
     {
-      path: 'Monorale-ExtraBold.woff',
+      path: '../../../fonts/Monorale-ExtraBold.woff',
       weight: '800',
       style: 'normal',
     },
     {
-      path: 'Monorale-ExtraBoldItalic.woff',
+      path: '../../../fonts/Monorale-ExtraBoldItalic.woff',
       weight: '800',
       style: 'italic',
     },
     {
-      path: 'Monorale-Black.woff',
+      path: '../../../fonts/Monorale-Black.woff',
       weight: '900',
       style: 'normal',
     },
     {
-      path: 'Monorale-BlackItalic.woff',
+      path: '../../../fonts/Monorale-BlackItalic.woff',
       weight: '900',
       style: 'italic',
     },
