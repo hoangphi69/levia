20e086d1d1048212842a9db0a099ac3c06cb1778
Hoang Phi <69hoangphi69@gmail.com>
2025-05-05 00:13:38 +0700

    refactor local font imports

--- src/app/(public)/(site)/layout.tsx
+++ src/app/(public)/(site)/layout.tsx
@@ -1,112 +1,11 @@
 import '@/app/globals.css';
 import { Toaster } from '@/components/shadcn/sonner';
+import { monorale, shImogenAgnes } from '@/fonts';
 import type { Metadata } from 'next';
 import FAQ from '../_components/layout/faq';
 import Footer from '../_components/layout/footer';
 import FooterContact from '../_components/layout/footer-contact';
 import Navbar from '../_components/layout/navbar';
-import localFont from 'next/font/local';
-
-const shImogenAgnes = localFont({
-  variable: '--font-sh-imogen-agnes',
-  src: '../../../fonts/SHImogenAgnes.woff',
-});
-
-const monorale = localFont({
-  variable: '--font-monorale',
-  src: [
-    {
-      path: '../../../fonts/Monorale-Thin.woff',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ThinItalic.woff',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraLight.woff',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraLightItalic.woff',
-      weight: '200',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Light.woff',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-LightItalic.woff',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Regular.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-Italic.woff',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Medium.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-MediumItalic.woff',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-SemiBold.woff',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-SemiBoldItalic.woff',
-      weight: '600',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Bold.woff',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-BoldItalic.woff',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraBold.woff',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraBoldItalic.woff',
-      weight: '800',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Black.woff',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-BlackItalic.woff',
-      weight: '900',
-      style: 'italic',
-    },
-  ],
-});
 
 export const metadata: Metadata = {
   title: 'Levia',
--- src/app/(public)/admin/layout.tsx
+++ src/app/(public)/admin/layout.tsx
@@ -1,104 +1,7 @@
 import ThemeProvider from '@/app/(private)/admin/_components/theme/theme-provider';
 import '@/app/globals.css';
+import { monorale } from '@/fonts';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import { shImogenAgnes } from '@/fonts';
-
-const monorale = localFont({
-  variable: '--font-monorale',
-  src: [
-    {
-      path: '../../../fonts/Monorale-Thin.woff',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ThinItalic.woff',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraLight.woff',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraLightItalic.woff',
-      weight: '200',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Light.woff',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-LightItalic.woff',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Regular.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-Italic.woff',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Medium.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-MediumItalic.woff',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-SemiBold.woff',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-SemiBoldItalic.woff',
-      weight: '600',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Bold.woff',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-BoldItalic.woff',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraBold.woff',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-ExtraBoldItalic.woff',
-      weight: '800',
-      style: 'italic',
-    },
-    {
-      path: '../../../fonts/Monorale-Black.woff',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/Monorale-BlackItalic.woff',
-      weight: '900',
-      style: 'italic',
-    },
-  ],
-});
 
 export const metadata: Metadata = {
   title: 'Đăng nhập',
@@ -126,7 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="mx-auto max-w-screen-xl antialised">
+      <body
+        className={`mx-auto max-w-screen-xl antialised ${monorale.variable}`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
